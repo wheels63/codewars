5a31744832f5743cc65344027f8dff043819b06d
Taeho Lee <31860067+wheels63@users.noreply.github.com>
2022-11-20 12:12:15 -0500

    finished a 7kyu kata

--- kyu7/findTheMiddleElement.js
+++ kyu7/findTheMiddleElement.js
@@ -1,4 +1,5 @@
 // https://www.codewars.com/kata/545a4c5a61aa4c6916000755/train/javascript
+// know the differences between a shallow and deep copy. or mutating arrays in place. 
 
 function gimme (triplet) {
 
