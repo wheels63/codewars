0bd8e86940c1d9501a75ea3a2ebf31906b5b9267
Taeho Lee <31860067+wheels63@users.noreply.github.com>
2022-11-20 12:29:18 -0500

    complete anagram detection

---
+++ kyu7/anagramDetection.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/529eef7a9194e0cbc1000255/solutions/javascript
+
+var isAnagram = function(test, original) {
+
+    return test.toLowerCase().split("").sort().join("") === original.toLowerCase().split("").sort().join("")
+    
+};
